a5ad4ead18beecd263bae85428d8707457e51c25
Jasnoor Kaur <HarjjotSinghh@gmail.com>
2023-05-24 22:17:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TopperHQ
+ A website which displays all the important questions from each chapter of every subject.
---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect
+import asyncio
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+async def page_not_found(e):
+    return redirect('/welcome')
+
+
+@app.route('/welcome')
+async def welcome():
+    return render_template('welcome.html')
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.ensure_future(app.run(debug=True)))
